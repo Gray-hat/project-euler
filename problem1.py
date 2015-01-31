97595b6ebb9402afe9d11721ac1b3ff796d639b5
Ian Kosen <ikosenn@gmail.com>
2015-01-31 16:09:58 +0300

    Add a second way to solve the problem more efficiently

--- problem1.py
+++ problem1.py
@@ -11,4 +11,20 @@ for i in range(0, below):
 
 total = sum(store)
 
-print "The sum of all multiples of 3 or 5 below %d is %d"%(below, total)	
+print "The sum of all multiples of 3 or 5 below %d is %d"%(below, total)
+
+
+def arithmetic_progression(numbers,upper_bound):
+    '''Given an list of numbers the function gets the sum of their
+        multiples in the range provided'''
+            total = 0
+                if isinstance(numbers,list):
+                for number in numbers:
+                    number_terms = (upper_bound/number) if upper_bound > number else 0
+                        temp = (number_terms/2.0)* (2 * number + ((number_terms -1) * number))
+                        total += temp
+                
+    return total	
+        return False
+
+print "The sum is", arithmetic_progression([3,5],999)
